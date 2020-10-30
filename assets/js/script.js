a3c46adb55aea0caae88f82124496d63a25a5780
tmstafford <tatumstafford1@gmail.com>
2020-10-30 18:30:04 -0500

    five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,18 +16,15 @@ let getCurrentWeather = function(cityName) {
     fetch(apiUrl)
     .then(function(response) {
         response.json().then(function(data) {
-            console.log(response);
-            console.log(apiUrl);
-            console.log(data.main.temp);
             
             displayWeatherEl.classList = "currentWeather";
             cityTitleEl.textContent = cityName;
             currentDateEl.textContent = "(" + todayDate + ")";
 
             let getIcon = data.weather[0].icon;
-            console.log(getIcon);
             let iconUrl = "http://openweathermap.org/img/wn/" + getIcon + ".png";
             weatherIconEl.setAttribute("src", iconUrl);
+            weatherIconEl.setAttribute("alt", data.weather[0].description);
 
             let currentTemp = data.main.temp;
             temperatureEl.textContent = "Temperature: " + currentTemp + " °F";
@@ -41,11 +38,70 @@ let getCurrentWeather = function(cityName) {
             let uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
             fetch(uvUrl).then(function(response) {
                 response.json().then(function(data) {
-                    console.log(data.value);
                     let currentUV = data.value;
                     UVIndexEl.textContent = "UV Index: " + currentUV;
                 });
             });
+        });
+    });
+};
+
+let showForecastEl = document.querySelector("#showForecast");
+let forecastEl = document.querySelector("#fiveDayForecast");
+
+let getForecast = function(cityName) {
+    let forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
+    fetch(forecastUrl)
+    .then(function(response) {
+        response.json().then(function(data) {
+            let showForecast = document.createElement("h2");
+            showForecast.textContent = "5-Day Forecast";
+            showForecastEl.appendChild(showForecast);
+            
+            for (i=0; i < data.list.length; i++) {
+
+                if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+                    console.log(data.list[i]);
+
+                    let cardDiv = document.createElement("div");
+                    cardDiv.classList = "card bg-primary col-md-2";
+                    forecastEl.appendChild(cardDiv);
+                    let cardBody = document.createElement("div");
+                    cardBody.setAttribute("id", "cardBody");
+                    cardBody.classList = "card-body";
+                    cardDiv.appendChild(cardBody);
+
+                    let date = new Date(data.list[i].dt*1000);
+                    let month = date.getMonth()+ 1;
+                    let day = date.getDate();
+                    if (day < 10) {
+                        day = "0" + date.getDate();
+                    }
+                    let year = date.getFullYear();
+
+                    let forecastDate = document.createElement("h5");
+                    forecastDate.classList = "card-title";
+                    forecastDate.textContent = "(" + month + "/" + day + "/" + year + ")";
+                    cardBody.appendChild(forecastDate);
+
+                    let getForecastIcon = data.list[i].weather[0].icon;
+                    let getForecastUrl = "http://openweathermap.org/img/wn/" + getForecastIcon + ".png";
+                    let forecastIcon = document.createElement("img");
+                    forecastIcon.setAttribute("src", getForecastUrl);
+                    forecastIcon.setAttribute("alt", data.list[i].weather[0].description);
+                    cardBody.appendChild(forecastIcon);
+
+                    let forecastTemp = document.createElement("p");
+                    forecastTemp.classList = "card-text";
+                    forecastTemp.textContent = "Temp: " + data.list[i].main.temp + " °F";
+                    cardBody.appendChild(forecastTemp);
+
+                    let forecastHumidity = document.createElement("p");
+                    forecastHumidityclassList = "card-text";
+                    forecastHumidity.textContent = "Humidity: " + data.list[i].main.humidity + " %";
+                    cardBody.appendChild(forecastHumidity);
+                }
+            };
         });
     });
 };
@@ -59,9 +115,9 @@ let formSubmitHandler = function(event) {
     let city = searchCityEl.value.trim();
     if (city) {
         getCurrentWeather(city);
+        getForecast(city);
         searchCityEl.value = "";
     }
-}
+};
 
-// getCurrentWeather();
 submitBtnEl.addEventListener("click", formSubmitHandler);
